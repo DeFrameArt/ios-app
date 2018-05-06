edd80b45eec0a3d94eb1750dad445105119139c8
MashaK <kochetygova.m@husky.meu.edu>
2018-05-06 15:38:06 -0400

    Please check - this should work . and have the same code as Master 8

--- DeFrameApp/Pods/MaterialComponents/components/schemes/Typography/src/MDCLegacyFontScheme.h
+++ DeFrameApp/Pods/MaterialComponents/components/schemes/Typography/src/MDCLegacyFontScheme.h
@@ -16,13 +16,17 @@
 
 #import <UIKit/UIKit.h>
 
+#pragma mark - Soon to be deprecated
+
 /**
  A font scheme comprised of set of UIFonts that are associated with various text styles.
 
  By apply the scheme to various components using themers one can set fonts on a single instance
- of a components or on all instances of that component using the UIAppearance proxy.
+ of a components.
 
  If a font in a scheme is nil, a component should use its default font.
+
+ @warning This class will soon be deprecated. Consider using MDCTypographyScheming instead.
  */
 @protocol MDCFontScheme <NSObject>
 
@@ -69,6 +73,8 @@
 
 /**
  A basic font scheme implements the MDCFontScheme protocol.
+
+ @warning This class will soon be deprecated. Consider using MDCTypographyScheme instead.
  */
 @interface MDCBasicFontScheme : NSObject <MDCFontScheme>
 
--- DeFrameApp/Pods/Target Support Files/Pods-DeFrameApp-DeFrameAppTests/Pods-DeFrameApp-DeFrameAppTests-frameworks.sh
+++ DeFrameApp/Pods/Target Support Files/Pods-DeFrameApp-DeFrameAppTests/Pods-DeFrameApp-DeFrameAppTests-frameworks.sh
@@ -154,7 +154,6 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/MotionTransitioning/MotionTransitioning.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SDWebImage/SDWebImage.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SwiftyJSON/SwiftyJSON.framework"
-  install_framework "${PODS_ROOT}/pyze-sdk-iOS/Pyze.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/ApiAI/ApiAI.framework"
@@ -177,7 +176,6 @@ if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/MotionTransitioning/MotionTransitioning.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SDWebImage/SDWebImage.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SwiftyJSON/SwiftyJSON.framework"
-  install_framework "${PODS_ROOT}/pyze-sdk-iOS/Pyze.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
