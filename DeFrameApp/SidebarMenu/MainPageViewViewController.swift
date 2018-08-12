79fdf1daaaa721cae0bd514e684a6902bed1b775
MashaK <kochetygova.m@husky.meu.edu>
2018-08-11 21:11:49 -0400

    main page additions

--- DeFrameApp/SidebarMenu/MainPageViewViewController.swift
+++ DeFrameApp/SidebarMenu/MainPageViewViewController.swift
@@ -74,12 +74,112 @@ class MainPageViewViewController: UIViewController,UITableViewDelegate, UITableV
         
         
     }
+   
+    @IBOutlet weak var logoImage: UIImageView!
+    let maxHeaderHeight: CGFloat = 88;
+    @IBOutlet weak var titleDeframe: UILabel!
+    let minHeaderHeight: CGFloat = 44;
     @IBOutlet weak var backGroundColor: UIImageView!
+    @IBOutlet weak var headerHeightConstraint: NSLayoutConstraint!
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         UIApplication.shared.statusBarStyle = .lightContent
         self.navigationController?.isNavigationBarHidden = true
+       // self.headerHeightConstraint.constant = 0
+     //  self.headerHeightConstraint.constant = self.maxHeaderHeight
+      //  updateHeader()
     }
+   /*
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let scrollDiff = scrollView.contentOffset.y - self.previousScrollOffset
+        
+        let absoluteTop: CGFloat = 0;
+        let absoluteBottom: CGFloat = scrollView.contentSize.height - scrollView.frame.size.height;
+        
+        let isScrollingDown = scrollDiff > 0 && scrollView.contentOffset.y > absoluteTop
+        let isScrollingUp = scrollDiff < 0 && scrollView.contentOffset.y < absoluteBottom
+        
+        if canAnimateHeader(scrollView) {
+            
+            // Calculate new header height
+            var newHeight = self.headerHeightConstraint.constant
+            if isScrollingDown {
+                newHeight = max(self.minHeaderHeight, self.headerHeightConstraint.constant - abs(scrollDiff))
+            } else if isScrollingUp {
+                newHeight = min(self.maxHeaderHeight, self.headerHeightConstraint.constant + abs(scrollDiff))
+            }
+            
+            // Header needs to animate LocationList
+            if newHeight != self.headerHeightConstraint.constant {
+                self.headerHeightConstraint.constant = newHeight
+                self.updateHeader()
+                self.setScrollPosition(self.previousScrollOffset)
+            }
+            
+            self.previousScrollOffset = scrollView.contentOffset.y
+        }
+    }
+    
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        self.scrollViewDidStopScrolling()
+    }
+    
+    func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+        if !decelerate {
+            self.scrollViewDidStopScrolling()
+        }
+    }
+    
+    func scrollViewDidStopScrolling() {
+        let range = self.maxHeaderHeight - self.minHeaderHeight
+        let midPoint = self.minHeaderHeight + (range / 2)
+        
+        if self.headerHeightConstraint.constant > midPoint {
+            self.expandHeader()
+        } else {
+            self.collapseHeader()
+        }
+    }
+    
+    func canAnimateHeader(_ scrollView: UIScrollView) -> Bool {
+        // Calculate the size of the scrollView when header is collapsed
+        let scrollViewMaxHeight = scrollView.frame.height + self.headerHeightConstraint.constant - minHeaderHeight
+        
+        // Make sure that when header is collapsed, there is still room to scroll
+        return scrollView.contentSize.height > scrollViewMaxHeight
+    }
+    
+    func collapseHeader() {
+        self.view.layoutIfNeeded()
+        UIView.animate(withDuration: 0.2, animations: {
+            self.headerHeightConstraint.constant = self.minHeaderHeight
+            self.updateHeader()
+            self.view.layoutIfNeeded()
+        })
+    }
+    
+    func expandHeader() {
+        self.view.layoutIfNeeded()
+        UIView.animate(withDuration: 0.2, animations: {
+            self.headerHeightConstraint.constant = self.maxHeaderHeight
+            self.updateHeader()
+            self.view.layoutIfNeeded()
+        })
+    }
+    
+    func setScrollPosition(_ position: CGFloat) {
+        self.mainTable.contentOffset = CGPoint(x: self.mainTable.contentOffset.x, y: position)
+    }
+    
+    func updateHeader() {
+        let range = self.maxHeaderHeight - self.minHeaderHeight
+        let openAmount = self.headerHeightConstraint.constant - self.minHeaderHeight
+        let percentage = openAmount / range
+        
+        //self.titleTopConstraint.constant = -openAmount + 10
+        self.logoImage.alpha = percentage
+    } */
+    
    // @IBOutlet weak var headerView: UIView!
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -95,11 +195,24 @@ class MainPageViewViewController: UIViewController,UITableViewDelegate, UITableV
         gradientLayer.frame = self.view.bounds
         name.layer.addSublayer(gradientLayer)
     }
-   
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+
+        if(segue.identifier == "LocationList"){
+            let museumListViewController = segue.destination as! ListTableViewController
+            museumListViewController.museum=allMuseums
+        
+        }
+    }
+    
    
     @IBOutlet weak var collectionCities: UICollectionView!
 
+   
     
+    var previousScrollOffset: CGFloat = 0;
+    
+    @IBOutlet weak var titleTopConstraint: NSLayoutConstraint!
+   
     var indexP=0
     
     
